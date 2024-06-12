12d592f2d34762b284f7690226a5df8e1e2bd7f9
Diego <yodaad@gmail.com>
2024-06-12 15:39:16 -0500

    Multiple recursion file added

--- src/multiple_recursion.py
+++ src/multiple_recursion.py
@@ -1,4 +1,4 @@
-# Multiple Recursion in Big O notation is a rule that states that the multiple of two functions is the product of the two functions.
+# Multiple Recursion in Big O notation is a rule that states that the multiple of two functions is the product of the two functions
 
 def fibonacci(n):
     if n == 0 or n == 1:
